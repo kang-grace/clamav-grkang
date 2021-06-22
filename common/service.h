522dd23361a64395bd0b7d0b7ce78a3229b51d51
Grace Kang <grkang@cisco.com>
2021-06-22 12:58:12 -0400

    Fixed comment

--- common/service.h
+++ common/service.h
@@ -2,7 +2,7 @@
  *  Copyright (C) 2013-2021 Cisco Systems, Inc. and/or its affiliates. All rights reserved.
  *  Copyright (C) 2007-2013 Sourcefire, Inc.
  *
- *  Authors: ??
+ *  Authors: Gianluigi Tiesi
  *
  *  This program is free software; you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License version 2 as
